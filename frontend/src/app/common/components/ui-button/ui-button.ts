e44d72404de98c07ac262a3be965b177ef8c162e
mjbalouta <mjbalouta@gmail.com>
2026-04-22 12:24:23 +0100

    feat: added a button component and a folder structure

---
+++ frontend/src/app/common/components/ui-button/ui-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-button',
+  imports: [],
+  template: ` <p>ui-button works!</p> `,
+  styles: ``,
+})
+export class UiButton {}
