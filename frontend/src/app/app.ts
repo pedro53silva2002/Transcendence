d4c3f81b84019ccd5483e802c6bfa8dc2e38bbdb
mjoao-fr <mjbalouta@gmail.com>
2026-04-22 23:23:33 +0100

    feat: added a reusable popup component with personalized title and message

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,36 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UiButton } from './common/components/ui-button/ui-button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationPopUp } from './shared/pop-up/confirmation-pop-up/confirmation-pop-up';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UiButton],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor(private dialog: MatDialog) {}
+  
+  openPopUp(): void {
+	const dialogRef = this.dialog.open(ConfirmationPopUp, {
+		data: {
+			title: "ola",
+			message: "coucou"
+		}
+	});
+
+	dialogRef.afterClosed().subscribe(response => {
+		if (response === true)
+			console.log('ola');
+		else
+			console.log('adeus');
+	});
+  }
 }
+
+
--- frontend/src/app/common/components/ui-button/ui-button.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ui-button',
-  imports: [],
-  template: ` <p>ui-button works!</p> `,
-  styles: ``,
-})
-export class UiButton {}
