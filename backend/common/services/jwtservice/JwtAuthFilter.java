c717f50c6d0df31dfd72df0c84592cffedba9225
Diogo Ferreira <155843454+DadiaZ24@users.noreply.github.com>
2026-04-28 15:27:07 +0100

    feat: added jwt (#15)
    * feat: added jwt
    
    * fix: added make test through docker

---
+++ backend/common/services/jwtservice/JwtAuthFilter.java
@@ -0,0 +1,60 @@
+package services.jwtservice;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.slf4j.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthFilter.class);
+
+    public JwtAuthFilter(JwtProvider jwtProvider)
+    {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+            String header = request.getHeader("Authorization");
+
+            if (header == null || !header.startsWith("Bearer "))
+            {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String token = header.substring(7);
+
+            if (!jwtProvider.validateToken(token)) 
+            {
+                log.warn("Invalid Jwt token received");
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            UUID userId = jwtProvider.getUserIdFromToken(token);
+            String role = jwtProvider.getRoleFromToken(token);
+
+            List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userId.toString(), null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            filterChain.doFilter(request, response);
+        }
+}
