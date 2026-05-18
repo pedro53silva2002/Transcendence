d6f642d5cbd898f498312a3616cfab460042956a
mjoao-fr <mjbalouta@gmail.com>
2026-05-18 23:23:10 +0100

    feat: started the dashboard design - created a reusable component for the dashboard-card; - created the nav-bar for this page; - added the four cards to the page + the title and subtitle

--- frontend/src/app/core/layout/app-home/app-home.component.ts
+++ frontend/src/app/core/layout/app-home/app-home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
-import { BreadcrumbComponent } from '../../../shared/components/breadcrumb/breadcrumb.component';
+import { LanguageButtonComponent } from "../../../shared/components/language-button/language-button.component";
+import { TranslocoModule } from '@jsverse/transloco';
+import { MatButtonModule } from '@angular/material/button';
+import { DashboardCardComponent } from "../../../shared/dashboard-card/dashboard-card.component";
 
 @Component({
   selector: 'app-app-home',
-  imports: [NavbarComponent,
-			RouterOutlet,
-			BreadcrumbComponent
- 			],
+  imports: [RouterOutlet, LanguageButtonComponent, TranslocoModule, MatButtonModule, DashboardCardComponent],
   templateUrl: './app-home.component.html',
   styleUrl: './app-home.component.scss',
 })
