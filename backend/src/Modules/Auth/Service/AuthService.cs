b349743b5e935c3f2e02d15aceeda20445e4009d
peferrei <peferrei@Transcendence.myguest.virtualbox.org>
2026-05-19 11:36:02 +0100

    fix: updating authService validation

--- backend/src/Modules/Auth/Service/AuthService.cs
+++ backend/src/Modules/Auth/Service/AuthService.cs
@@ -1,11 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-using System.Xml;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Trippie.Common.Services.Authentication.Jwt;
 using Trippie.Common.Services.Authentication.Security;
 using Trippie.Modules.Auth.Dtos;
-using Trippie.Modules.Auth.Model;
+using Trippie.Common.Services.GlobalExceptionHandler.Exceptions;
 
 namespace Trippie.Modules.Auth.Service;
 
@@ -13,11 +9,11 @@ public sealed class AuthService(UserService userService, IJwtTokenService jwt)
 {
 	public async Task<AuthResponseDto> Register(RegisterDto dto, CancellationToken ct = default)
 	{
-		if (dto.Username is null) throw new ValidationException($"Username cannot be empty.");
-		if (dto.Email is null) throw new ValidationException($"Email cannot be empty.");
-		if (dto.Password is null) throw new ValidationException($"Password cannot be null.");
-		if (dto.Password.Length < 8) throw new ValidationException($"Password needs to have at least 8 characters.");
-		if (!dto.Email.Contains('@')) throw new ValidationException($"Email needs to have one @.");
+		if (dto.Username is null) throw new ValidationException("email", "Username cannot be empty.", "Username cannot be empty.");
+		if (dto.Email is null) throw new ValidationException("email", "Email cannot be empty.", "Email cannot be empty.");
+		if (dto.Password is null) throw new ValidationException("password", "Password cannot be null.", "Password cannot be null.");
+		if (dto.Password.Length < 8) throw new ValidationException("password", "Password needs to have at least 8 characters.", "Password needs to have at least 8 characters.");
+		if (!dto.Email.Contains('@')) throw new ValidationException("email", "Email needs to have one @.", "Email needs to have one @.");
 
 		CreateUserDto createUserDto = new()
 		{
@@ -49,18 +45,18 @@ public sealed class AuthService(UserService userService, IJwtTokenService jwt)
 
 	public async Task<AuthResponseDto> Login(LoginDto dto, CancellationToken ct = default)
 	{
-		if (dto.Email is null) throw new ValidationException($"Email cannot be empty.");
-		if (dto.Password is null) throw new ValidationException($"Password cannot be null.");
-		if (dto.Password.Length < 8) throw new ValidationException($"Password needs to have at least 8 characters.");
-		if (!dto.Email.Contains('@')) throw new ValidationException($"Email needs to have one @.");
+		if (dto.Email is null) throw new ValidationException("email", "Email cannot be empty.", "Email cannot be empty.");
+		if (dto.Password is null) throw new ValidationException("password", "Password cannot be null.", "Password cannot be null.");
+		if (dto.Password.Length < 8) throw new ValidationException("password", "Password needs to have at least 8 characters.", "Password needs to have at least 8 characters.");
+		if (!dto.Email.Contains('@')) throw new ValidationException("email", "Email needs to have one @.", "Email needs to have one @.");
 
 
-		var foundUser = await userService.GetByEmail(dto.Email, ct) ?? throw new ValidationException($"Email or password is incorrect.");
+		var foundUser = await userService.GetByEmail(dto.Email, ct) ?? throw new ValidationException("email", "Email is incorrect.", "Email is incorrect.");
 
 		string? passwordHash = await userService.GetPasswordByEmail(foundUser.Email, ct);
-		if (passwordHash is null) throw new ValidationException($"Password cannot be empty in database.");
+		if (passwordHash is null) throw new ValidationException("password", "Password cannot be empty in database.", "Password cannot be empty in database.");
 		if (new BCryptPasswordHasher().Verify(dto.Password, passwordHash) == false)
-			throw new ValidationException($"Password invalid.");
+			throw new ValidationException("password", "Password invalid.", "Password invalid.");
 		var (Token, ExpiresAtUtc) = jwt.GenerateToken(new JwtUserClaims
 		{
 			UserId = foundUser.Id,
