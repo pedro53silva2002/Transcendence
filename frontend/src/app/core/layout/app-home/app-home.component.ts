487972e92035cae8c91ffaf74ce70c1cb42479a6
Maria João Balouta <165146704+mjbalouta@users.noreply.github.com>
2026-05-21 16:12:32 +0100

    Dashboard frontend (#54)
    * fix: combined all the navbar components into just one: navbar
    
    * feat: added stat cards and slogan to the bottom of the dashboard page
    
    * fix: box-shadow of all the buttons
    
    * fix: added searchbar to the navbar for the dashboard page
    
    * fix: fixed en.json file
    
    * feat: adding flags to the language button
    
    * feat: added username to the dashboard
    
    * fix: changed the page's title
    
    * fix: changed from guestGuard to authGuard

--- frontend/src/app/core/layout/app-home/app-home.component.ts
+++ frontend/src/app/core/layout/app-home/app-home.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
-import { LanguageButtonComponent } from "../../../shared/components/language-button/language-button.component";
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { MatButtonModule } from '@angular/material/button';
 import { DashboardCardComponent } from "../../../shared/dashboard-card/dashboard-card.component";
 import { NavbarComponent } from '../navbar/navbar.component';
+import { AuthService } from '../../logic/services/auth.service';
+import { MeDto } from '../../feature/auth/AuthDtos';
 
 @Component({
-  selector: 'app-app-home',
-  imports: [TranslocoModule,
-            MatButtonModule,
-            DashboardCardComponent,
-            NavbarComponent],
-  templateUrl: './app-home.component.html',
-  styleUrl: './app-home.component.scss',
+	selector: 'app-app-home',
+	imports: [TranslocoModule,
+		MatButtonModule,
+		DashboardCardComponent,
+		NavbarComponent],
+	templateUrl: './app-home.component.html',
+	styleUrl: './app-home.component.scss',
 })
-export class AppHomeComponent {}
+export class AppHomeComponent implements OnInit {
+	private readonly authService = inject(AuthService);
+	public user = signal<MeDto | null>(null);
+
+	ngOnInit(): void {
+		try {
+			const response = this.authService.me();
+			if (response) {
+				this.user.set(response);
+			}
+		} catch (error) {
+			console.error('Erro ao carregar utilizador:', error);
+			this.user.set(null);
+		}
+	}
+}
