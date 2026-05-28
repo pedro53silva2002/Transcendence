dd2238e38e9798be800e08016b3e645181a08f1c
mjbalouta <mjbalouta@gmail.com>
2026-05-28 17:27:08 +0100

    feat: added trip services and trip dto

---
+++ frontend/src/app/core/feature/plan-a-trip/dtos/trip.dto.ts
@@ -0,0 +1,52 @@
+export enum TripVisibility {
+    Private = 0,
+    Public = 1,
+    FriendsOnly = 2
+}
+
+//what i send in http post request to create a trip
+export interface CreateTripDto {
+    tripName: string;
+    description?: string;
+    destination?: string;
+    startDate: string;
+    endDate: string;
+    budget: number;
+    visibility: TripVisibility;
+    createdBy: number;
+}
+
+//what i receive in http get
+export interface TripDto {
+    id: number;
+    tripName: string;
+    description?: string;
+    destination?: string;
+    startDate: string;
+    endDate: string;
+    budget: number;
+    visibility: TripVisibility;
+    createdBy: number;
+    createdAt: string;
+    updatedAt?: string;
+}
+
+//what i send in http put request to update a trip
+export interface UpdateTripDto {
+    id: number;
+    tripName: string;
+    description?: string;
+    destination?: string;
+    startDate: string;
+    endDate: string;
+    budget: number;
+    visibility: TripVisibility;
+}
+
+//what i receive in http get when i search for trips
+export type TripSearchFieldsDto = {
+    tripName?: string;
+    destination?: string;
+}
+
+export type TripOrderByFieldsDto = 'startDate' | 'createdAt' | 'tripName';
---
+++ frontend/src/app/core/feature/plan-a-trip/services/trip.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { ApiResponse, BaseApiService } from '../../../logic/services/base-api.service';
+import { CreateTripDto, TripDto, TripOrderByFieldsDto, TripSearchFieldsDto, UpdateTripDto } from '../dtos/trip.dto';
+import { Observable } from 'rxjs';
+import { CursorPage, SearchParams, searchToQuery } from '../../../logic/services/search.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TripService extends BaseApiService {
+
+  //Create a trip (POST)
+  public create(dto: CreateTripDto): Observable<ApiResponse<TripDto>> {
+    return this._post<TripDto>(`/api/trips`, dto);
+  }
+
+  //Get trips (GET)
+  public search(search: SearchParams<TripSearchFieldsDto, TripOrderByFieldsDto>): Observable<ApiResponse<CursorPage<TripDto>>> {
+    const query = searchToQuery(search);
+    return this._get<CursorPage<TripDto>>(`api/trips/search?q=${query}`);
+  }
+
+  //Get trips by id (GET)
+  public getById(id: number): Observable<ApiResponse<TripDto>> {
+    return this._get<TripDto>(`api/trips/${id}`);
+  }
+
+  //Update a trip (PUT)
+  public update (id: number,dto: UpdateTripDto): Observable<ApiResponse<TripDto>> {
+    return this._put<TripDto>(`/api/trips/${id}`, dto);
+  }
+
+  //Delete a trip (DELETE)
+  public delete(id: number): Observable<ApiResponse<void>> {
+    return this._delete<void>(`/api/trips/${id}`);
+  }
+}
+
