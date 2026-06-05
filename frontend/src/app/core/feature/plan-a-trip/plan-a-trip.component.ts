5a7e04bceb19bd29de885dba5091ff59a8ffdf85
peferrei <peferrei@Transcendence.myguest.virtualbox.org>
2026-06-05 15:29:44 +0100

    Fix: removed frontend errors Pedro Silva

--- frontend/src/app/core/feature/plan-a-trip/plan-a-trip.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-plan-a-trip',
-  imports: [],
-  templateUrl: './plan-a-trip.component.html',
-  styleUrl: './plan-a-trip.component.scss',
-})
-export class PlanATripComponent {}
--- frontend/transloco.config.ts
+++
@@ -1,9 +0,0 @@
-import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
-
-const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'public/i18n/',
-  langs: ['en', 'pt', 'es'],
-  keysManager: {},
-};
-
-export default config;
