3eeeac3d19122dde4df47ec95887e8f722bd4314
mjoao-fr <mjbalouta@gmail.com>
2026-06-16 00:34:32 +0100

    feat: created the stat cards component in order to use both in user-dashboard and profile; feat: started the profile implementation

--- frontend/src/app/core/feature/profile/profile.component.ts
+++ frontend/src/app/core/feature/profile/profile.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
+import { TranslocoModule } from '@jsverse/transloco';
+import { AuthService } from '../auth/services/auth.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, of } from 'rxjs';
+import { StatCardsComponent } from "../../../shared/stat-cards/stat-cards.component";
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [MatIcon, TranslocoModule, StatCardsComponent, MatAnchor],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export default class ProfileComponent {}
+export default class ProfileComponent {
+	
+	private authService = inject(AuthService);
+
+	private mockApiResponse = {
+		data : {
+			displayName: 'Cristiano Ronaldo',
+			username: 'cr7',
+			profilePhotoUrl: 'images/globe.png'
+			// profilePhotoUrl: null
+		}
+	};
+	
+	public user = toSignal(
+		// this.authService.me().pipe(
+		of(this.mockApiResponse).pipe(
+			map(response => response.data)
+		)
+	);
+}
