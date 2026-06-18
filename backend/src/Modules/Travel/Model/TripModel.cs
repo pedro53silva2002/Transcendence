ab83d3b70bbdc14f5b33ad065c3013d8543c214b
peferrei <peferrei@Transcendence.myguest.virtualbox.org>
2026-06-18 16:12:40 +0100

    fix: mofications on durantion of trips. Now the beginning date counts aswell and you can have a trip on the same day. Pedro Silva

--- backend/src/Modules/Travel/Model/TripModel.cs
+++ backend/src/Modules/Travel/Model/TripModel.cs
@@ -76,7 +76,7 @@ public sealed class TripModel(AppDbContext db)
 			Id = 0,
 			TripName = dto.TripName,
 			Description = dto.Description,
-			Duration = (dto.EndDate - dto.StartDate).Days,
+			Duration = (dto.EndDate - dto.StartDate).Days + 1,
 			StartDate = dto.StartDate,
 			EndDate = dto.EndDate,
 			Budget = dto.Budget == 0 ? 0 : dto.Budget, // --- IGNORE ---
@@ -193,7 +193,7 @@ public sealed class TripModel(AppDbContext db)
 		if (dto.Description is not null) trip.Description = dto.Description;
 		if (dto.StartDate != trip.StartDate) trip.StartDate = dto.StartDate;
 		if (dto.EndDate != trip.EndDate) trip.EndDate = dto.EndDate;
-		if ((dto.EndDate - dto.StartDate).Days != trip.Duration) trip.Duration = (dto.EndDate - dto.StartDate).Days;
+		if (((dto.EndDate - dto.StartDate).Days + 1) != trip.Duration) trip.Duration = (dto.EndDate - dto.StartDate).Days + 1;
 		if (dto.Budget is not 0) trip.Budget = dto.Budget;
 		trip.UpdatedAt = DateTime.UtcNow;
 
