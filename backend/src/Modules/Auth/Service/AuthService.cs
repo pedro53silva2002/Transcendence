52501fb26ecbdabb6efd2b65cfa2086d7a835532
dadiaz <diogo-uds@hotmail.com>
2026-06-29 09:04:28 +0100

    feat: add friend

--- backend/src/Modules/Auth/Service/AuthService.cs
+++ backend/src/Modules/Auth/Service/AuthService.cs
@@ -42,6 +42,7 @@ public sealed class AuthService(UserService userService, IJwtTokenService jwt, A
 
         var me = new MeDto
         {
+            Id = userId,
             Username = user.Username,
             DisplayName = user.DisplayName,
             Email = user.Email,
